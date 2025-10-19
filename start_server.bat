5f635bb6a8acb99e570c695800e4201165c68fec
Wen <wenyin@nyu.edu>
2025-10-20 06:53:15 +0800

    version 2.0
    todo: 1. 时序图 2. 一键下载

--- start_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Student Grades Report Server...
-echo.
-echo Open your browser and go to http://localhost:8000
-echo.
-echo Press CTRL+C to stop the server
-echo.
-python -m http.server 8000
-pause
